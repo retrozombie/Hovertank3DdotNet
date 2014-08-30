46709f1c1b1c610164bfa5e4826aa0a827633fc7
retrozombie <retrozombie.mail@gmail.com>
2014-08-30 13:01:51 +0100

    Adding initial conversions.

---
+++ src/hovertank3D/tilept.cs
@@ -0,0 +1,36 @@
+﻿/* Hovertank 3-D Source Code
+ * Copyright (C) 1993-2014 Flat Rock Software
+ * Copyright (C) 2014 Andy Stewart
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Hovertank3DdotNet
+{
+    struct tilept
+    {
+        public short x;
+
+        public short y;
+
+        public override string ToString()
+        {
+            return string.Concat("x = ", x.ToString(), ", y = ", y.ToString());
+        }
+    }
+}
